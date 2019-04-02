1df61483b70caccfc67f4538a5d56aca9187850c
sato akihide <aki@sato.local>
2019-04-02 17:39:32 +0900

    tasksテーブルのカラム名修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_02_081648) do
+ActiveRecord::Schema.define(version: 2019_04_02_083511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.text "contents"
-    t.date "limit"
+    t.text "content"
+    t.date "deadline"
     t.string "priority"
     t.string "status"
     t.datetime "created_at", null: false

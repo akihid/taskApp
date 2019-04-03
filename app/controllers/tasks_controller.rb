bc270e78d3dde8a45a7ab8dc84abae2e82eb4efc
sato akihide <aki@sato.local>
2019-04-03 11:13:54 +0900

    新規登録機能実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,36 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def show
   end
 
-  def create
+  def new
+    @task = Task.new
   end
 
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      flash[:success] = "新規登録完了"
+      redirect_to tasks_path
+    else
+      render 'new'
+    end
+  end
+
+
   def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title , :contetnt , :deadline_at , :priority , :status)
   end
 end

132bbc4c090ee6f43af5867ad66edbb2e34ec7f2
sato akihide <aki@sato.local>
2019-04-03 11:45:48 +0900

    編集および確認画面実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,7 @@
 class TasksController < ApplicationController
+
+  before_action :set_task , only:[:edit ,:update ,:show]
+
   def index
     @tasks = Task.all
   end
@@ -7,7 +10,11 @@ class TasksController < ApplicationController
   end
 
   def new
-    @task = Task.new
+    if paragitms[:back]
+      @task = Task.new(task_params)
+    else
+      @task = Task.new
+    end
   end
 
   def create
@@ -21,16 +28,31 @@ class TasksController < ApplicationController
     end
   end
 
+  def confirm
+    @task = Task.new(task_params)
+  end
+
+
 
   def edit
   end
 
   def update
+    if @task.update(task_params)
+      flash[:success] = "編集完了"
+      redirect_to tasks_path
+    else
+      render 'edit'
+    end
   end
 
   private
 
   def task_params
-    params.require(:task).permit(:title , :contetnt , :deadline_at , :priority , :status)
+    params.require(:task).permit(:title , :content , :deadline_at , :priority , :status)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end

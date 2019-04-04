6856906c5fc76e051e5994b20a1dcc6edbf15b29
sato akihide <aki@sato.local>
2019-04-04 12:45:24 +0900

    タスク一覧を作成日時の順番で並び替え、およびそれのテスト実装

---
+++ spec/factories/task.rb
@@ -0,0 +1,26 @@
+FactoryBot.define do
+
+  factory :task  do
+    title { 'test_task_01' }
+    content { 'testtesttest01' }
+    deadline_at { '2019-04-01' }
+    priority { '高' }
+    status { '未着手' }
+  end
+
+  factory :second_task, class: Task do
+    title { 'test_task_02' }
+    content { 'testtesttest02' }
+    deadline_at { '2019-04-02' }
+    priority { '高' }
+    status { '未着手' }
+  end
+
+  factory :third_task, class: Task do
+    title { 'test_task_03' }
+    content { 'testtesttest03' }
+    deadline_at { '2019-04-03' }
+    priority { '高' }
+    status { '未着手' }
+  end
+end

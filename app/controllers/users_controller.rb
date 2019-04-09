9cd471eecb4f0bdcb66bbd9e33f0efe69c054b1f
sato akihide <aki@sato.local>
2019-04-09 13:13:40 +0900

    会員新規登録機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+
+before_action :set_user , only:[:show]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = '会員登録しました'
+      redirect_to tasks_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name , :mail , :password , :password_confirmation)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :name , presence: true , length: {maximum:20}
   validates :mail , presence: true , format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  before_validation{:mail.downcase!}
+  before_validation{:mail.downcase}
   has_secure_password
   validates :password , presence: true , length: {minimum:6}
 end

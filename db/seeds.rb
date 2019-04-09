3ea693b2964cffa923f08eab7ef3b8ffdd02f331
sato akihide <aki@sato.local>
2019-04-09 12:27:48 +0900

    ユーザ概念の導入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,17 @@
-100.times do |n|
-  title = "test#{n}"
-  content = "test"
+# 100.times do |n|
+#   title = "test#{n}"
+#   content = "test"
   
-  Task.create!(title: title,
-               content: content,
+#   Task.create!(title: title,
+#                content: content,
+#                )
+# end
+
+1.times do |n|
+  name = "user#{n}"
+  mail = "user#{n}@co.jp"
+  
+  User.create!(name: name,
+               mail: mail,
                )
 end

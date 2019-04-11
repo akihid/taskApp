30e69ed8271f5e695b283cf482d2d09d4e6213e4
sato akihide <aki@sato.local>
2019-04-11 12:00:37 +0900

    usersテーブルにrole追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_09_032445) do
+ActiveRecord::Schema.define(version: 2019_04_11_025823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2019_04_09_032445) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "role", default: false
     t.index ["mail"], name: "index_users_on_mail", unique: true
   end
 

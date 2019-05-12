c1d1bbd9c2d2191e50f827b8ddf680c71cfc6193
sato akihide <aki@sato.local>
2019-05-12 19:47:18 +0900

    Add: groupモデル作成（外部キーにuser)

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_12_060557) do
+ActiveRecord::Schema.define(version: 2019_05_10_092729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
 
   create_table "labels", force: :cascade do |t|
     t.string "word"
@@ -51,5 +59,6 @@ ActiveRecord::Schema.define(version: 2019_04_12_060557) do
     t.index ["mail"], name: "index_users_on_mail", unique: true
   end
 
+  add_foreign_key "groups", "users"
   add_foreign_key "tasks", "users"
 end

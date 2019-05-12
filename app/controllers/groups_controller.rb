e3d34d602e8d9707da4b1137733391763cf71bfa
sato akihide <aki@sato.local>
2019-05-13 00:31:45 +0900

    Add: グループ参照・削除機能実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,10 @@
 class GroupsController < ApplicationController
+
+  before_action :set_group , only:[:edit, :update, :destroy, :show]
+
+  def index
+    @groups = Group.all
+  end
   def new
     @group = Group.new
   end
@@ -8,15 +14,36 @@ class GroupsController < ApplicationController
 
     if @group.save
       flash[:success] = t('msg.new_group_complete')
-      redirect_to tasks_path
+      redirect_to groups_path
     else
       render 'new'
     end
+  end
+
+  def edit
+
+  end
+
+  def update
+    @group.update(group_params)
+    redirect_to groups_path , notice: "グループ「#{@group.name}」を更新しました。"
+  end
+
+  def show
+  end
+
+  def destroy
+    @group.destroy
+    redirect_to groups_path , notice: "グループ「#{@group.name}」を削除しました。"
   end
 
   private
 
   def group_params
     params.require(:group).permit(:name)
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end

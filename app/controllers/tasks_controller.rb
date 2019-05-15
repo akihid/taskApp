c0ec8518082d306540af92a7a0d48966818561e2
sato akihide <aki@sato.local>
2019-05-15 19:25:26 +0900

    Add: タスクに添付ファイルをつけた

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -66,7 +66,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title , :content , :deadline_at , :priority , :status)
+    params.require(:task).permit(:title , :content , :deadline_at , :priority , :status, :image)
   end
 
   def set_task
